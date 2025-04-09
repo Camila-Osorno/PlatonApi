95dbaba54b9255886c4d9d9fef27e713e21709b9
Camila Osorno Suarez <camila_osorno23221@elpoli.edu.co>
2025-04-09 18:40:50 -0500

    Implementación de clase Inscripcion y estructura de modelo

--- src/main/java/models/Inscripcion.java
+++ src/main/java/models/Inscripcion.java
@@ -1,4 +1,17 @@
 package models;
 
 public class Inscripcion {
+
+    public class Inscripcion {
+        private int id;
+        private String estudiante;
+        private String documento;
+        private String carrera;
+        private String asignatura;
+        private String semestre;
+        private String fechaInscripcion;
+        private String estado;
+
+        // Constructor, getters y setters
+    }
 }
